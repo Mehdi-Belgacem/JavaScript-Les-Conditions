498d764bb3caeebf919b475dcae1a097b7e87eee
Mehdi-Belgacem <72203119+Mehdi-Belgacem@users.noreply.github.com>
2020-11-03 14:02:31 +0100

    correction de la parti 2

--- exercice5/script.js
+++ exercice5/script.js
@@ -1,17 +1,24 @@
-let day= 7
+let day= Number(prompt ('Indiquez un chiffre'));
 switch(day){
     case 1:
-        alert(`Lundi`)
+        alert('Lundi')
+        break;
     case 2:
-        alert(`Mardi`)
+        alert('Mardi')
+        break;
     case 3:
-        alert(`Mercredi`)
+        alert('Mercredi')
+        break;
     case 4:
-        alert(`Jeudi`)
+        alert('Jeudi')
+        break;
     case 5:
-        alert(`Vendredi`)
+        alert('Vendredi')
+        break;
     case 6:
         alert('Samedi')
+        break;
     case 7:
-        alert(`Dimanche`)
+        alert('Dimanche')
+        break;
 }
